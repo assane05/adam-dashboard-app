bf33b482309ec093ed6ec39e9ce42cd1471a04bf
assane05e <gueyeazou201629@gmaile.com>
2023-04-19 18:01:18 +0000

    article

--- src/components/tasks/tasks.js
+++ src/components/tasks/tasks.js
@@ -168,68 +168,6 @@ function Tasks() {
           </div>
         </div>
         <div className="row">
-          {/* <div className="col-lg-4">
-            <div class="card shadow mb-4">
-              <div class="g-3">
-                <div class="col mt-3">
-                  <label class="form-label">Titre</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="titre du document"
-                  />
-                </div>
-                <div class="g-3">
-                  <div class="col mt-3">
-                    <label class="form-label">Date</label>
-                    <input type="date" class="form-control" />
-                  </div>
-                </div>
-              </div>
-              <div class="g-3">
-                <div class="col mt-3">
-                  <label class="form-label">Label</label>
-
-                  <select
-                    class="form-select"
-                    aria-label="Default select example"
-                  >
-                    <option selected>image</option>
-                    {/* <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option> 
-                  </select>
-                </div>
-              </div>
-              <div class="g-3">
-                <div class="col mt-3">
-                  <label class="form-label">image</label>
-                  <input
-                    type="file"
-                    class="form-control"
-                    placeholder="img"
-                    title="choisir une image"
-                  />
-                </div>
-              </div>
-              <div className="g-3">
-                <div class="col mt-3 mb-4">
-                  <label for="exampleFormControlTextarea1" class="form-label">
-                    Commentaire
-                  </label>
-                  <textarea
-                    class="form-control"
-                    id="exampleFormControlTextarea1"
-                    rows="3"
-                  ></textarea>
-                </div>
-              </div>
-              <div className="m-auto">
-                <button className="btn btn-primary mb-4">Ajouter</button>
-              </div>
-            </div>
-          </div> */}
-
           <div className="col-lg-12">
             <div className="row">
               <div className="col-lg-3">
